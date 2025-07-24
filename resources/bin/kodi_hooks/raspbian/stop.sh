9d5b90f01515f78d789af79cd609180c634213db
João Carlos Bastos Portela <jcbastosportela@gmail.com>
2025-07-25 01:08:53 +0200

    Update stop.sh
    For xbain (raspbian) the stop hook was causing kodi to send Off command to the TV.
    
    This attempts to fix that.

--- resources/bin/kodi_hooks/raspbian/stop.sh
+++ resources/bin/kodi_hooks/raspbian/stop.sh
@@ -1,2 +1,3 @@
 # Stop kodi
-killall --wait kodi.bin || true
+killall -9 kodi.bin # hard kill so it does not send HDMI CEC Off command to the TV 
+sudo stop xbmc      # stop the service so it does not auto respawn kodi
